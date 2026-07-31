b28ececd5db73b7fce5fddf43737ab168de6d190
Gilkleber Medeiros <gikimedei2018@gmail.com>
2026-07-31 19:25:24 -0300

    test: add application init tests.

---
+++ test/test_init.py
@@ -0,0 +1,26 @@
+"""
+Initialization tests.
+"""
+
+from unittest import TestCase
+from fastapi.testclient import TestClient
+from sqlalchemy import Engine
+
+from app.main import app
+
+
+class TestAppInitTestCase(TestCase):
+  def setUp(self):
+    self.client = TestClient(app)
+    return super().setUp()
+
+  def test_server_ping(self):
+    response = self.client.get("/ping")
+
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.json()["message"], "pong")
+
+  def test_can_get_db_engine(self):
+    from app.db import engine
+
+    self.assertIsInstance(engine, Engine)
