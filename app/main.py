289238eac80ad8c440dceaec894e3ae886d6bd16
Gilkleber Medeiros <gikimedei2018@gmail.com>
2026-07-31 16:49:21 -0300

    feat: init fastapi server.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,21 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI(
+    title="IoTAlert", summary="API de Telemetria industrial. (Projeto Pessoal)"
+)
+
+
+@app.get("/ping")
+def ping():
+    return {"message": "pong"}
+
+
 def main():
     print("Hello from iotalert!")
+
+    uvicorn.run(app, host="localhost", port=8000)
 
 
 if __name__ == "__main__":
